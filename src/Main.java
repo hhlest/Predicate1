fe9be6487131ab74c3cf502a79d0fa575f1e9bec
Waminms <67516571+Waminms@users.noreply.github.com>
2023-02-14 12:01:25 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> list1 = new ArrayList<>(List.of("Flash", "black", "final", "Fox", "orange"));
+        System.out.println(list1);
+
+        Predicate<String> prs = (a) -> a.startsWith("F");
+        list1.removeIf(prs);
+
+        System.out.println(list1);
+    }
+}
